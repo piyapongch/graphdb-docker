3dd4245954db00619f66e42a8d90564685478812
piyapongch <charoenw@ualberta.ca>
2020-04-28 11:37:55 -0600

    Remove comments

--- free-edition/init/init_graphdb.sh
+++ free-edition/init/init_graphdb.sh
@@ -7,7 +7,5 @@ sleep 10
 echo "creating repositories..."
 cd /opt/graphdb/init
 ./create_repositories.sh
-#cp owlim.properties /opt/graphdb/home/data/repositories/fedora/storage/
-#cp owlim.properties /opt/graphdb/home/data/repositories/audit/storage/
 
 echo "graphdb started!"
